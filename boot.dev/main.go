832f7b9feb7219dd7fd825439e95971b2388157a
Piyush Raj <piyushrajkhg@gmail.com>
2024-09-18 00:59:21 +0530

    cleanup for concurrency

--- boot.dev/main.go
+++ boot.dev/main.go
@@ -67,51 +67,51 @@
 // 	fmt.Println(globalValue.a)
 // }
 
-package main
+// package main
 
-import (
-	"errors"
-)
+// import (
+// 	"errors"
+// )
 
-type customer struct {
-	id      int
-	balance float64
-}
+// type customer struct {
+// 	id      int
+// 	balance float64
+// }
 
-type transactionType string
+// type transactionType string
 
-const (
-	transactionDeposit    transactionType = "deposit"
-	transactionWithdrawal transactionType = "withdrawal"
-)
+// const (
+// 	transactionDeposit    transactionType = "deposit"
+// 	transactionWithdrawal transactionType = "withdrawal"
+// )
 
-type transaction struct {
-	customerID      int
-	amount          float64
-	transactionType transactionType
-}
+// type transaction struct {
+// 	customerID      int
+// 	amount          float64
+// 	transactionType transactionType
+// }
 
-// Don't touch above this line
+// // Don't touch above this line
 
-// ?
-func updateBalance(c *customer, t transaction) error {
+// // ?
+// func updateBalance(c *customer, t transaction) error {
 
-	if !(t.transactionType == transactionDeposit || t.transactionType == transactionWithdrawal) {
-		return errors.New("unknown transaction type")
-	}
+// 	if !(t.transactionType == transactionDeposit || t.transactionType == transactionWithdrawal) {
+// 		return errors.New("unknown transaction type")
+// 	}
 
-	if t.transactionType == transactionDeposit {
-		c.balance += t.amount
-		return nil
-	}
+// 	if t.transactionType == transactionDeposit {
+// 		c.balance += t.amount
+// 		return nil
+// 	}
 
-	if t.amount > c.balance {
-		return errors.New("insufficient funds")
-	}
+// 	if t.amount > c.balance {
+// 		return errors.New("insufficient funds")
+// 	}
 
-	c.balance -= t.amount
-	return nil
-}
+// 	c.balance -= t.amount
+// 	return nil
+// }
 
-func main() {
-}
+// func main() {
+// }
