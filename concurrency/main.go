e0b91530a43e1e6db2477245932bb2bc512e914b
Piyush Raj <piyushrajkhg@gmail.com>
2024-09-19 01:45:37 +0530

    added code for fanout using select, channels

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,5 +1,70 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	parent := make(chan int)
+
+	exit := make(chan bool)
+
+	child1 := make(chan int, 1)
+	child2 := make(chan int, 1)
+	child3 := make(chan int, 1)
+
+	// producer
+	go func() {
+		i := 0
+		for i < 10 {
+			parent <- i
+			fmt.Printf("Produced: %d\n", i)
+			time.Sleep(time.Millisecond * 100)
+			i++
+		}
+
+		close(parent)
+	}()
+
+	// consumer
+	go func() {
+		for data := range parent {
+
+			select {
+			case child1 <- data:
+				fmt.Println("Sent to child1: ", data)
+			case child2 <- data:
+				fmt.Println("sent to child2: ", data)
+			case child3 <- data:
+				fmt.Println("sent to child3", data)
+			}
+		}
+
+		close(child1)
+		close(child2)
+		close(child3)
+		exit <- true
+	}()
+
+	// child consumers
+	go func() {
+		for data := range child1 {
+			fmt.Println("child1 received: ", data)
+		}
+	}()
+
+	go func() {
+		for data := range child2 {
+			fmt.Println("child2 received: ", data)
+		}
+	}()
+
+	go func() {
+		for data := range child3 {
+			fmt.Println("child3 received: ", data)
+		}
+	}()
+
+	<-exit
 }
